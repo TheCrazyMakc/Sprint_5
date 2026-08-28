85f3e14db02b00d228c1036172d410da155ace99
@ochumelyi_maks <only-max@yandex.ru>
2026-08-28 09:33:27 +0300

    add class TestRegistration 1-1 test

--- tests/test_1-1_auto_registration.py
+++ tests/test_1-1_auto_registration.py
@@ -6,8 +6,9 @@ from selenium.webdriver.support.ui import WebDriverWait
 from locators import Locators
 from helpers import generate_unique_email, generate_password
 
+class TestRegistration:
 
-def test_go_to_personal_account(driver):
+  def test_go_to_personal_account(self, driver):
     # Генерируем новый email перед каждым тестом
     unique_email = generate_unique_email()
     unique_password = generate_password(6)  # Генерируем пароль из 6 символов
@@ -43,3 +44,40 @@ def test_go_to_personal_account(driver):
     )
     
     print("✅ Тест пройден: Успешная регистрация")
+
+# def test_go_to_personal_account(driver):
+#     # Генерируем новый email перед каждым тестом
+#     unique_email = generate_unique_email()
+#     unique_password = generate_password(6)  # Генерируем пароль из 6 символов
+    
+#     # переход по кнопке "Войти в аккаунт"
+#     WebDriverWait(driver, 3).until(
+#         EC.element_to_be_clickable((Locators.LOGIN_BUTTON_MAIN))
+#     ).click()
+    
+#     # переход по ссылке "зарегистрироваться"
+#     WebDriverWait(driver, 3).until(
+#         EC.element_to_be_clickable((Locators.REGISTER_LINK))
+#     ).click()
+
+#     # Выполни авторизацию
+#     name_form = driver.find_element(*Locators.NAME_INPUT)
+#     name_form.clear()
+#     name_form.send_keys("TestName")
+
+#     mail_form = driver.find_element(*Locators.EMAIL_INPUT)
+#     mail_form.clear()
+#     mail_form.send_keys(unique_email)  # ← Используем сгенерированный email
+
+#     password_form = driver.find_element(*Locators.PASSWORD_INPUT)
+#     password_form.clear()
+#     password_form.send_keys(unique_password)
+
+#     driver.find_element(*Locators.REGISTER_BUTTON).click()
+
+#     # Проверяем, что регистрация успешная (появилась кнопка "Войти")
+#     WebDriverWait(driver, 10).until(
+#         EC.element_to_be_clickable(Locators.LOGIN_BUTTON)
+#     )
+    
+#     print("✅ Тест пройден: Успешная регистрация")
